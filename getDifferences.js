de363a9cc25d1c95ca1f09f0512212fd9080a097
Semen <IvanovSA2309@gmail.com>
2024-01-15 17:46:41 +0400

    step 7 v2 (still doesnt work)

--- getDifferences.js
+++ getDifferences.js
@@ -12,18 +12,18 @@ function compare(contentOfFile1, contentOfFile2, iterator = 1){
   const keys = _.union(keys1, keys2)
   console.log(keys)
   
-  keys.map((key) => { 
+  keys.sort().map((key) => { 
     if (contentOfFile1[key] instanceof Object) {
       if (key in contentOfFile1 && key in contentOfFile2) { 
-        resultString += `${'  '.repeat(i - 1)}  ${key}: ${compare(contentOfFile1[key], contentOfFile2[key], )}\n`
+        resultString += `${'  '.repeat(i)}  ${key}: ${compare(contentOfFile1[key], contentOfFile2[key], i)}\n`
       } else if (key in contentOfFile1 && !(key in contentOfFile2)) { 
-        resultString += `${'  '.repeat(i - 1)}- ${key}: ${compare(contentOfFile1[key], {})}\n`
+        resultString += `${'  '.repeat(i)}- ${key}: ${compare(contentOfFile1[key], {}, i)}\n`
       }
       else if (!(key in contentOfFile1) && key in contentOfFile2) { 
-        resultString += `${'  '.repeat(i - 1)}+ ${key}: ${compare({}, contentOfFile2[key])}\n`
+        resultString += `${'  '.repeat(i)}+ ${key}: ${compare({}, contentOfFile2[key], i)}\n`
       } else if (JSON.stringify(contentOfFile1[key]) !== JSON.stringify(contentOfFile2[key])) {
-        resultString += `${'  '.repeat(i - 1)}- ${key}: ${compare(contentOfFile1[key], contentOfFile2[key])}\n`
-        resultString += `${'  '.repeat(i - 1)}+ ${key}: ${compare(contentOfFile1[key], contentOfFile2[key])}\n`
+        resultString += `${'  '.repeat(i)}- ${key}: ${compare(contentOfFile1[key], contentOfFile2[key], i)}\n`
+        resultString += `${'  '.repeat(i)}+ ${key}: ${compare(contentOfFile1[key], contentOfFile2[key], i)}\n`
       }
     } else {
       if (contentOfFile1[key] === contentOfFile2[key]) { 
@@ -41,7 +41,7 @@ function compare(contentOfFile1, contentOfFile2, iterator = 1){
 
   
 
-  return `{\n${resultString}${'  '.repeat(i - 1)}}`
+  return `{\n${resultString}${'  '.repeat(i )}}`
 }
 
 export default function getDifferences(filepath1, filepath2) {
