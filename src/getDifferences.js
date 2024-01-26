10916adb945ec10f26055a45a279c462d7580b3e
Semen <IvanovSA2309@gmail.com>
2024-01-27 03:24:47 +0400

    plain format added

--- src/getDifferences.js
+++ src/getDifferences.js
@@ -1,11 +1,12 @@
+import comparePlain from './getDifferencesPlain.js';
 import parser from './parser.js';
-import compare from './stylish.js';
+// import compare from './stylish.js';
 
 
-export default function getDifferences(filepath1, filepath2, format = 'stylish') {
+export default function getDifferences(filepath1, filepath2) {
   const contentOfFile1 = parser(filepath1);
   const contentOfFile2 = parser(filepath2);
 
 
-  return  compare(contentOfFile1, contentOfFile2)
+  return  comparePlain(contentOfFile1, contentOfFile2)
 }
---
+++ src/getDifferencesPlain.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+
+export default function comparePlain(contentOfFile1, contentOfFile2, parentLine = '') {
+  let resultString = '';
+
+  const keys1 = Object.keys(contentOfFile1);
+  const keys2 = Object.keys(contentOfFile2);
+  const keys = _.union(keys1, keys2);
+  
+  keys.sort().map((key) => {
+    const currentParentLine = parentLine ? `${parentLine}.${key}` : key;
+
+    if (key in contentOfFile1 && key in contentOfFile2) {
+      if (contentOfFile1[key] instanceof Object && contentOfFile2[key] instanceof Object) {
+        resultString += comparePlain(contentOfFile1[key], contentOfFile2[key], currentParentLine);
+      } else if (contentOfFile1[key] instanceof Object && !(contentOfFile2[key] instanceof Object)) {
+        resultString += `Property '${currentParentLine}' was updated. From [complex value] to '${contentOfFile2[key]}'\n`;
+      } else if (!(contentOfFile1[key] instanceof Object) && contentOfFile2[key] instanceof Object) {
+        resultString += `Property '${currentParentLine}' was updated. From '${contentOfFile1[key]}' to [complex value]\n`;
+      } else if (!(contentOfFile1[key] instanceof Object) && !(contentOfFile2[key] instanceof Object)) {
+        if (contentOfFile1[key] === contentOfFile2[key]) {
+          resultString += '';
+        } else {
+          resultString += `Property '${currentParentLine}' was updated. From '${contentOfFile1[key]}' to '${contentOfFile2[key]}'\n`;
+        }
+      }
+    } else if (key in contentOfFile1 && !(key in contentOfFile2)) {
+      resultString += `Property '${currentParentLine}' was removed\n`;
+    } else if (!(key in contentOfFile1) && key in contentOfFile2) {
+      const valueString = contentOfFile2[key] instanceof Object ? '[complex value]' : '${contentOfFile2[key]}';
+      resultString += `Property '${currentParentLine}' was added with value: ${valueString}\n`;
+    }
+  });
+
+  return resultString;
+}
