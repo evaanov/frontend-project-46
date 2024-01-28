ce1e940d6de91c1064a9f4b4bccfe7dbaa506db0
Semen <IvanovSA2309@gmail.com>
2024-01-28 19:57:52 +0400

    aaa

--- __tests__/getDifferences.test.js
+++ __tests__/getDifferences.test.js
@@ -12,21 +12,21 @@ const json = readFile('json');
 const plain = readFile('plain');
 const stylish = readFile('stylish');
 
-test('stylish formatter', () => {
+test('stylish', () => {
   const expectResultJSON = getDifferences('__fixtures__/file1.json', '__fixtures__/file2.json');
   const expectResultYML = getDifferences('__fixtures__/file1.yaml', '__fixtures__/file2.yaml');
   expect(expectResultJSON).toEqual(stylish);
   expect(expectResultYML).toEqual(stylish);
 });
 
-test('plain formatter', () => {
+test('plain', () => {
   const expectResultJSON = getDifferences('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain');
   const expectResultYML = getDifferences('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'plain');
   expect(expectResultJSON).toEqual(plain);
   expect(expectResultYML).toEqual(plain);
 });
 
-test('json formatter', () => {
+test('json', () => {
   const expectResultJSON = getDifferences('__fixtures__/file1.json', '__fixtures__/file2.json', 'json');
   const expectResultYML = getDifferences('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'json');
   expect(expectResultJSON).toEqual(json);
